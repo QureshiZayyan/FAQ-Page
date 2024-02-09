1eb080fa92a9d1b96287a5c3de537b28192b9864
Zayyan <qureshizayyan49@gmail.com>
2024-02-10 04:01:05 +0530

    make some changes

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ show.forEach((e) => {
 });
 
 let child = document.querySelectorAll('.child');
-Array.from(child).forEach((element, index) => {
-    element.addEventListener('click', () => {
-        if (show[index].style.display === 'none') {
-            show[index].style.display = 'block';
-        }
 
-        else if (show[index].style.display === 'block') {
-            show[index].style.display = 'none';
+child.forEach((element) => {
+    element.addEventListener('click', () => {
+        const isOpen = element.classList.contains('open');
+
+        child.forEach((el) => {
+            el.classList.remove('open')
+            el.nextElementSibling.style.display = 'none';
+            el.lastElementChild.style = 'none';
+        });
+
+        if (!isOpen) {
+            element.classList.add('open');
+            element.lastElementChild.classList.add('open');
+            element.nextElementSibling.classList.toggle('opens');
+            element.lastElementChild.classList.toggle('open');
+            element.nextElementSibling.style.display = 'block';
+            element.lastElementChild.style = 'transform: rotate(48deg) translateX(2.9px)';
         }
-    })
-})
+    });
+});
